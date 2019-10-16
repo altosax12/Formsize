fd9a47332c4b8e3773f7073c89a27569f92e177e
David Morgan <domorganj@yahoo.com>
2019-10-16 12:54:37 -0500

    add Lab 7

--- Form/Site.Master.designer.cs
+++ Form/Site.Master.designer.cs
@@ -13,15 +13,6 @@ namespace Form {
     public partial class Site {
         
         /// <summary>
-        /// head control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
-        
-        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
@@ -29,6 +20,15 @@ namespace Form {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
         
         /// <summary>
         /// Menu1 control.
---
+++ Form/product.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page MasterPageFile="~/Site.Master" Language="C#" AutoEventWireup="true"  CodeBehind="product.aspx.cs" Inherits="Form.product" %>
+
+
+
+ 
+<asp:Content ID="Content2" ContentPlaceHolderID="Body" runat="server">  
+   
+    <asp:GridView ID="GridView1" runat="server">
+
+        
+</asp:GridView>
+   
+</asp:Content>  
