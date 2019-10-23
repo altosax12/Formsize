d711885ef14a17a049c0a4ca28fbe35c13a3bdd6
David Morgan <domorganj@yahoo.com>
2019-10-23 16:50:27 -0500

    some bootstrap added need more

--- Form/Site.Master.designer.cs
+++ Form/Site.Master.designer.cs
@@ -22,15 +22,6 @@ namespace Form {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// TextBox1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox1;
-        
-        /// <summary>
         /// Menu1 control.
         /// </summary>
         /// <remarks>
